fd137369f5f2dc9859862069fb7d53225a64bac9
matveeva-aa <adel.matveeva@gmail.com>
2025-09-28 15:49:56 +0300

    Task_02

---
+++ Task_2_1/AboutWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Task_2_1
+{
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
