41be01dcc174c48611857d5a76eaa7e3a9abaff1
matveeva-aa <adel.matveeva@gmail.com>
2025-10-18 17:09:04 +0300

    Task 6/1

---
+++ Task_6_1/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Task_6_1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
