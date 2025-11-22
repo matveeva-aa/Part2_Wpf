39181a5f3937b5affa20262aefbe65a609b603db
matveeva-aa <adel.matveeva@gmail.com>
2025-11-22 18:40:38 +0300

    Task_8_1

---
+++ Task_8_1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace Task_8_1
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> Products { get; } =
+            new ObservableCollection<Product>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Products.Add(new Product
+            {
+                Name = "Яблоки",
+                Price = 299.99m,
+                ImagePath = "Images/apple.jpg",
+                Category = ProductCategory.Food
+            });
+
+            Products.Add(new Product
+            {
+                Name = "Холодильник",
+                Price = 59999.99m,
+                ImagePath = "Images/fridge.jpg",
+                Category = ProductCategory.Appliances
+            });
+
+            Products.Add(new Product
+            {
+                Name = "Бананы",
+                Price = 199.99m,
+                ImagePath = "Images/bananas.png",
+                Category = ProductCategory.Food
+            });
+
+            DataContext = this;
+        }
+    }
+}
---
+++ Task_8_1/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_8_1
+{
+    public enum ProductCategory
+    {
+        Food,
+        Appliances
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImagePath { get; set; }
+        public ProductCategory Category { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Price} ₽, {Category})";
+        }
+    }
+}
